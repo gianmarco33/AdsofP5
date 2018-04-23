8948937da23853f34396a3ce841812c6d9226e19
eps <eps@2-27-64-111.mshome.net>
2018-04-23 17:49:16 +0200

    woloolo

--- src/grafos/Grafo.java
+++ src/grafos/Grafo.java
@@ -2,27 +2,55 @@
  * 
  */
 package grafos;
+import java.util.*;
 
 /**
  * @author gianmarco
  *
  */
 public abstract class Grafo<T> {
-	private final int id;
-	private T datos;
+
 	protected Map<Integer, Vertice<T>> vertices;
+	protected Map<Integer,Map<Integer,Integer>> arcos;
 	
 	
-	public Vertice<T> addVertice(T datos);
-	protected Vertice<T> addVertice(int id, T datos);
-	public List<Vertice<T>> getVertices();
-	public int getNumVertices();
+	public Vertice<T> addVertice(T datos){
+		Vertice<T> newVertice = new Vertice<T>(this.getNumVertices(),)
+		vertices.put(this.getNumVertices()+1, n)
+		return null;
+	}
+	protected Vertice<T> addVertice(int id, T datos){
+		return null;
+	}
+	public List<Vertice<T>> getVertices(){
+		return (List<Vertice<T>>) vertices.values();
+	}
+	public int getNumVertices() {
+		return vertices.keySet().size();
+	}
 	public abstract void addArco(Vertice<T> v1, Vertice<T> v2, double peso);
-	public boolean existeArco(Vertice<T> v1, Vertice<T> v2);
-	public int getNumArcos();
+	public boolean existeArco(Vertice<T> v1, Vertice<T> v2) {
+		Integer i = v1.getId();
+		Integer j = v2.getId();
+		if(arcos.get(i).get(j) != null)
+			return true;	
+		return false;
+	}
+	public int getNumArcos() {
+		Map<Integer,Integer> mapa;
+		int i = 0;
+		for(Integer k : arcos.keySet()) {
+			 mapa = arcos.get(k);
+			 for(Integer o : mapa.keySet())
+				 i++;
+		}
+		return i;	
+	}
 	public abstract double getPesoDe(Vertice<T> v1, Vertice<T> v2);
 	public abstract List<Vertice<T>> getVecinosDe(Vertice<T> v); // devuelve los vértices que tienen un arco con v
 	 // (en grafo dirigido, v ha de ser origen de los arcos)
-	public String toString(); // los vértices del grafo han de presentarse ORDENADOS POR IDENTIFICADOR
+	public String toString() {
+		return null;// los vértices del grafo han de presentarse ORDENADOS POR IDENTIFICADOR
+	}
 	
 }
--- src/grafos/Vertice.java
+++ src/grafos/Vertice.java
@@ -1,5 +1,13 @@
 package grafos;
 
-public class Vertice {
-
+public class Vertice<T> {
+	private final int id;
+	private T datos;
+	
+	public Vertice(int id1, T datos){
+		this.id = id1;
+		this.datos = datos;
+	}
+	public int getId() {return id;}
+	public T getDatos() {return datos;}
 }
